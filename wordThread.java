14ececa83bcfb3266332b1f2fba8707b9661f96a
VuTran-it <88641712+VuTran-it@users.noreply.github.com>
2021-10-03 08:23:28 +0700

    Create wordThread.java

---
+++ wordThread.java
@@ -0,0 +1,38 @@
+package Phat_trien_he_thong_tich_hop;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class wordThread  implements Runnable{
+    String ms;
+    public wordThread ( String s )
+    {
+        this.ms = s ;
+    }
+
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName() + " (Bắt đầu): " + ms);
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        System.out.println(Thread.currentThread().getName() + "(kết thúc)");
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newWorkStealingPool(5);
+
+        for ( int i = 0 ; i < 10 ; i++)
+        {
+            Runnable worker = new wordThread("" + i);
+            executor.execute(worker);
+        }
+        executor.shutdown();
+        while(!executor.isTerminated())
+        {
+        }
+        System.out.println("Kêt thúc POOL");
+    }
+}
