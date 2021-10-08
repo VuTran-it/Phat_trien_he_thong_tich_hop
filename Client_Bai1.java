24ae6d2ebf371e32ed81db0b5cbd90fec970e915
VuTran-it <88641712+VuTran-it@users.noreply.github.com>
2021-10-08 16:46:18 +0700

    Bai1

---
+++ Client_Bai1.java
@@ -0,0 +1,34 @@
+// package TH_Bai1;
+
+import java.io.*;
+import java.net.*;
+public class Client_Bai1 {
+	public static void main (String[] args){
+		// Tao ket noi
+		try{
+		Socket s = new Socket ("127.0.0.1",6430);
+		// Khai bao hai stream in-out
+		try{
+		InputStream is = s.getInputStream();
+		OutputStream os = s.getOutputStream();
+		while(true){
+			// Nhap vao tu ban phim
+			System.out.println("Nhap vao ky tu so: ");
+			int ch = System.in.read();
+			System.in.skip(2);
+			// Gui ky tu sang Server
+			os.write(ch);
+			if(ch=='@') break;
+			// Nhan ket qua tra ve
+			byte b[] = new byte[100];
+			int n = is.read(b);
+			String kqua = new String(b,0,n);
+			System.out.println("Nhan duoc: "+kqua);
+		}
+		// Dong noi ket
+		s.close();
+		}catch (UnknownHostException e ) {System.out.println("Co loi UnknownHostException!");};
+		}catch (IOException e) {System.out.println("Co loi IOException!");};
+		
+	}
+}
--- TCP_Client.java
+++ TCP_Client.java
@@ -7,22 +7,21 @@ public class TCP_Client {
     public final static int  ServerPort = 6789;
     public static void main(String[] args) throws InterruptedException {
         try {
-            Socket s = new Socket("127.0.0.1",ServerPort);
-            System.out.println("Client duoc tao");
+                Socket s = new Socket("127.0.0.1",ServerPort);
+                System.out.println("Client duoc tao");
 
-            InputStream is = s.getInputStream();
-            OutputStream os = s.getOutputStream();
-
-            for (int i = '0'; i < '9'; i++) {
-                os.write(i);
-
-
-                int ch = is.read();
-                System.out.println((char)ch);
-
-                Thread.sleep(2000);
-            }
-            s.close();
+                InputStream is = s.getInputStream();
+                OutputStream os = s.getOutputStream();
+                for (int i = '0'; i < '9'; i++) {
+                    os.write(i);
+    
+    
+                    int ch = is.read();
+                    System.out.println((char)ch);
+    
+                    Thread.sleep(2000);
+                }
+                s.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
