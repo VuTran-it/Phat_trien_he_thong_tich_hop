20ac47a24d378322defa1bdca998ef24cbd8bdf6
VuTran-it <88641712+VuTran-it@users.noreply.github.com>
2021-11-04 13:41:57 +0700

    up code

---
+++ VD/CalcInterface.java
@@ -0,0 +1,9 @@
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface CalcInterface extends Remote {
+    public int tong(int a, int b) throws RemoteException;
+
+    public int hieu(int a, int b) throws RemoteException;
+}
