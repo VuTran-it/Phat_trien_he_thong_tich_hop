bce561457a92989b02c9f8e1bda9408963e449d9
VuTran-it <88641712+VuTran-it@users.noreply.github.com>
2021-11-04 14:10:55 +0700

    up

--- TCPsongsong/server.java
+++ TCPsongsong/server.java
@@ -4,13 +4,13 @@ import java.util.*;
 
 public class server {
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(9001);
+        ServerSocket server = new ServerSocket(8888);
         System.out.println("Server connecting ");
-        System.out.println("server connected with port = 9001");
+        System.out.println("server connected with port = 8888");
         while (true) {
             Socket socket = server.accept();
             System.out.println("Chao Client " +socket.getPort() );
-            String str = "Chao client " + socket.getPort() + "\n" + "1. Ngay \n2. Gioi \n3. Ngay va gioi \n4. exit de thoat";
+            String str = "Chao client " + socket.getPort() + "\n" + "1. Gio \n2. Ngay \n3. Ngay va gioi \n4. exit de thoat";
             OutputStream os = socket.getOutputStream();
             os.write(str.getBytes());
             thread t = new thread(socket);
