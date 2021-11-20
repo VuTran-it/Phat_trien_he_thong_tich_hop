a5afd4ccdff37fc46806339d9941593b00e52f88
VuTran-it <88641712+VuTran-it@users.noreply.github.com>
2021-11-20 20:32:27 +0700

    up

--- test.java
+++ test.java
@@ -1,13 +1,41 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
 public class test {
     public static void main(String[] args) {
-        String chuoi = "10 70 100";
-        int sum = 0;
+        try {
+            String filename = "banner.txt";
+            String noidung = docfile(filename);
+            String chuoi = "\n"+ noidung;
 
-        for(String w:chuoi.split("\\s",0)){
-            System.out.println(w);
-            sum = sum + Integer.parseInt(w);
+            System.out.println("Noi dung : " + chuoi);
+        } catch (IOException e) {
+            //TODO: handle exception
         }
+    }
 
-        System.out.println("Tong chuoi bang : " + sum);
+    public static String docfile(String filename)throws IOException
+    {
+        File f = new File(filename);
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        String line = "";
+        String kq ="";
+        while (( line = br.readLine()) != null){
+            wait(200);
+            kq = kq.concat(line).concat("\n");
+        }
+        return kq;
+    }
+    public static void wait(int ms){
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
     }
 }
